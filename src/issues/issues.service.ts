eb2eeb552f26ca17da296752bf6acf963545798c
Sammy Abdulrahman <myoptionalps@gmail.com>
2023-12-07 23:35:49 +0300

    fix

--- src/issues/issues.service.ts
+++ src/issues/issues.service.ts
@@ -16,17 +16,22 @@ import { ProgramsService } from 'src/programs/programs.service';
 
 @Injectable()
 export class IssuesService {
-  constructor(@InjectModel(Issue.name) private issues: Model<Issue>, private programService:ProgramsService) {}
+  constructor(
+    @InjectModel(Issue.name) private issues: Model<Issue>,
+    private programService: ProgramsService,
+  ) {}
   async createIssue(
     @Res() response: Response,
     body: CreateIssueDTO,
     editor: mongoose.Schema.Types.ObjectId,
   ) {
     try {
-      const programData = await this.programService.findOne(body.processedProgram)
+      const programData = await this.programService.findOne(
+        body.processedProgram,
+      );
       const data = await this.issues.create({
         ...body,
-        department:programData.department,
+        department: programData.department,
         editor: editor,
       });
       return response.status(HttpStatus.CREATED).json({
@@ -43,7 +48,7 @@ export class IssuesService {
   async getAllIssues(@Res() response: Response, pagination: PaginationDto) {
     try {
       const totalData = await this.issues.countDocuments();
-      const {page,limit,...filtering} = pagination
+      const { page, limit, ...filtering } = pagination;
       const data = await this.issues
         .find(filtering)
         .skip(PaginationHelper.paginateQuery(pagination))
@@ -53,10 +58,10 @@ export class IssuesService {
         .populate({
           path: 'processedProgram',
           populate: [
-            { path: 'assignedCameraMen', select: '-userPin' }, 
+            { path: 'assignedCameraMen', select: '-userPin' },
             { path: 'equipments' },
-            { path: 'assignedEditor', select: '-userPin' }, 
-            { path: 'producerDetails',select:'-userPin' },
+            { path: 'assignedEditor', select: '-userPin' },
+            { path: 'producerDetails', select: '-userPin' },
           ],
         });
       return response
@@ -74,17 +79,25 @@ export class IssuesService {
     paginate: PaginationDto,
   ) {
     try {
-      const totalData =  await this.issues.countDocuments({editor:id})
+      const totalData = await this.issues.countDocuments({ editor: id });
       const data = await this.issues
         .find({ editor: id })
         .populate('editor', { password: 0 })
         .populate('department')
-        .populate('processedProgram')
+        .populate({
+          path: 'processedProgram',
+          populate: [
+            { path: 'assignedCameraMen', select: '-userPin' },
+            { path: 'equipments' },
+            { path: 'assignedEditor', select: '-userPin' },
+            { path: 'producerDetails', select: '-userPin' },
+          ],
+        })
         .skip(PaginationHelper.paginateQuery(paginate))
         .limit(paginate.limit);
       return response
         .status(HttpStatus.OK)
-        .json({ success: true, issues: data , totalData:totalData});
+        .json({ success: true, issues: data, totalData: totalData });
     } catch (error) {
       console.error(error);
       throw new InternalServerErrorException(ErrorMessage.internalServerError);
@@ -100,7 +113,15 @@ export class IssuesService {
         .find({ processedProgram: id })
         .populate('editor', { password: 0 })
         .populate('department')
-        .populate('processedProgram');
+        .populate({
+          path: 'processedProgram',
+          populate: [
+            { path: 'assignedCameraMen', select: '-userPin' },
+            { path: 'equipments' },
+            { path: 'assignedEditor', select: '-userPin' },
+            { path: 'producerDetails', select: '-userPin' },
+          ],
+        });
       return response
         .status(HttpStatus.OK)
         .json({ success: true, issues: data });
@@ -111,19 +132,28 @@ export class IssuesService {
   }
 
   async getById(response: Response, id: mongoose.Schema.Types.ObjectId) {
-    try { 
+    try {
       const data = await this.issues
         .findById(id)
         .populate('editor', { password: 0 })
         .populate('department')
-        .populate('processedProgram');
-      if(!data) throw new NotFoundException(ErrorMessage.programNotFound)
-      return response.status(HttpStatus.OK).json({success:true,issue:data})
-    }
-    catch (error) {
-      if (!(error instanceof InternalServerErrorException)) throw error
-      console.error(error)
-      throw new InternalServerErrorException(ErrorMessage.internalServerError)
+        .populate({
+          path: 'processedProgram',
+          populate: [
+            { path: 'assignedCameraMen', select: '-userPin' },
+            { path: 'equipments' },
+            { path: 'assignedEditor', select: '-userPin' },
+            { path: 'producerDetails', select: '-userPin' },
+          ],
+        });
+      if (!data) throw new NotFoundException(ErrorMessage.programNotFound);
+      return response
+        .status(HttpStatus.OK)
+        .json({ success: true, issue: data });
+    } catch (error) {
+      if (!(error instanceof InternalServerErrorException)) throw error;
+      console.error(error);
+      throw new InternalServerErrorException(ErrorMessage.internalServerError);
     }
   }
- }
+}
